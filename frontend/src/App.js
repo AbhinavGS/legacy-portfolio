a5273a5c2226e0132484ceb39d6db41979d08d2f
Abhinav Sorate <abhinavsorate01@gmail.com>
2022-07-26 10:01:48 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//custom components
+import { NavBar, Header, Footer } from "./components";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+import "./NavBar.scss";
+
+const NavBar = () => {
+  return (
+    <>
+      <Navbar bg="transparent" expand="lg" className="mb-3">
+        <Container fluid>
+          <Navbar.Brand className="navbar-brand">
+            <img src="logo192.png" alt="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-$"lg"`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-$"lg"`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-$"lg"`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-$"xl"`}>
+                Menu
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-center flex-grow-1 pe-3 navbar-links">
+                <Nav.Link className="nav-link" href="#action1">
+                  HOME
+                </Nav.Link>
+                <Nav.Link className="nav-link" href="#action2">
+                  ABOUT
+                </Nav.Link>
+                <Nav.Link className="nav-link" href="#action2">
+                  WORK
+                </Nav.Link>
+                <Nav.Link className="nav-link" href="#action2">
+                  SKILLS
+                </Nav.Link>
+                <Nav.Link className="nav-link" href="#action2">
+                  CONTACT
+                </Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBar;
