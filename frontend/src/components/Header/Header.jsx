c7a719ab46c2f2a315db053aea80cc07591d72b8
Abhinav Sorate <abhinavsorate01@gmail.com>
2022-07-26 14:24:43 +0530

    home session done

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Container, Typography, Button, Box, Grid, Stack } from "@mui/material";
+
+import "./Header.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <div>
+        <div className="container">
+          <div className="header-text-div">
+            <h3 className="header-text-h3">
+              HELLO.........................................
+            </h3>
+            <h1 className="header-text-h1">I AM ABHINAV SORATE</h1>
+            <h5 className="header-text-h5">FULL STACK DEVELOPER</h5>
+          </div>
+          <div className="header-image-div">
+            <img
+              className="header-image"
+              src="header-image-main.png"
+              alt="guy with a computer"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
